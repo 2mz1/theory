278d217a3e2ea1d864d18ead2e3e62148455310e
pursue503 <pursue503@naver.com>
2023-05-10 15:52:09 +0900

    Add Object Chapter5 152p

---
+++ objects/pursue503/chapter5/src/DiscountCondition.java
@@ -0,0 +1,27 @@
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class DiscountCondition {
+    private DisCountConditionType type;
+    private int sequence;
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public boolean isSatisfiedBy(Screening screening) {
+        if (type == DisCountConditionType.PERIOD) {
+            return isSatisfiedByPeriod(screening);
+        }
+        return isSatisfiedBySequence(screening);
+    }
+
+    private boolean isSatisfiedByPeriod(Screening screening) {
+        return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
+                startTime.compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                endTime.isAfter(screening.getWhenScreened().toLocalTime());
+    }
+
+    private boolean isSatisfiedBySequence(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
