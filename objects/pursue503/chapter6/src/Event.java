3cabc6c3ff5cb17ab38746c7c0053f0617f749ea
pursue503 <pursue503@naver.com>
2023-05-17 17:17:36 +0900

    Add Object Chapter6 legacy code

---
+++ objects/pursue503/chapter6/src/Event.java
@@ -0,0 +1,40 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Event {
+    private String subject;
+    private LocalDateTime from;
+    private Duration duration;
+
+    public Event(String subject, LocalDateTime from, Duration duration) {
+        this.subject = subject;
+        this.from = from;
+        this.duration = duration;
+    }
+
+    public boolean isSatisfied(RecurringSchedule schedule) {
+        if (from.getDayOfWeek() != schedule.getDayOfWeek() ||
+                !from.toLocalTime().equals(schedule.getFrom()) ||
+                !duration.equals(schedule.getDuration())) {
+            reschedule(schedule);
+            return false;
+        }
+        return true;
+    }
+
+    private void reschedule(RecurringSchedule schedule) {
+        from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)),
+                schedule.getFrom());
+        duration = schedule.getDuration();
+    }
+
+    private long daysDistance(RecurringSchedule schedule) {
+        return schedule.getDayOfWeek().getValue() - from.getDayOfWeek().getValue();
+    }
+
+    public static void main(String[] args) {
+        Event event = new Event("회의",
+                LocalDateTime.of(2019, 5, 8, 10, 30),
+                Duration.ofMillis(30));
+    }
+}
