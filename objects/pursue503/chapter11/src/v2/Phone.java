2d544bd60edc3a31fc823df819557cf116d891cd
pursue503 <pursue503@naver.com>
2023-06-25 14:41:32 +0900

    Add Chapter11 v2

---
+++ objects/pursue503/chapter11/src/v2/Phone.java
@@ -0,0 +1,24 @@
+package v2;
+
+import common.Call;
+import common.Money;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Phone {
+    private List<Call> calls = new ArrayList<>();
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+        for (Call call : calls) {
+            result = result.plus(calculateCallFee(call));
+        }
+        return afterCalculated(result);
+    }
+
+    protected abstract Money calculateCallFee(Call call);
+
+    protected abstract Money afterCalculated(Money fee);
+
+}
