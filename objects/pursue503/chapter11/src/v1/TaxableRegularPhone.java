de0ad5162a44118a8180eed1c86cf6e00d513319
pursue503 <pursue503@naver.com>
2023-06-25 14:19:24 +0900

    Add Chapter11 v1

---
+++ objects/pursue503/chapter11/src/v1/TaxableRegularPhone.java
@@ -0,0 +1,20 @@
+package v1;
+
+import common.Call;
+import common.Money;
+
+import java.time.Duration;
+
+public class TaxableRegularPhone extends RegularPhone {
+    private double taxRate;
+    public TaxableRegularPhone(Money amount, Duration seconds, double taxRate) {
+        super(amount, seconds);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        Money fee = super.calculateFee();
+        return fee.plus(fee.times(taxRate));
+    }
+}
