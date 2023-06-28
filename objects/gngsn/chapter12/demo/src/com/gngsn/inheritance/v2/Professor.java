8b396dfe1023e8801b200288ff71534deb7ebc76
gngsn <rudtjs4540@gmail.com>
2023-06-28 16:51:04 +0900

    add summing chapter 12 up

---
+++ objects/gngsn/chapter12/demo/src/com/gngsn/inheritance/v2/Professor.java
@@ -0,0 +1,20 @@
+package com.gngsn.inheritance.v2;
+
+import com.gngsn.inheritance.v1.Lecture;
+
+/**
+ *
+ */
+public class Professor {
+    private String name;
+    private Lecture lecture;
+
+    public Professor(String name, Lecture lecture) {
+        this.name = name;
+        this.lecture = lecture;
+    }
+    public String compileStatistics() {
+        return String.format("[%s] %s - Avg: %.1f", name,
+                lecture.evaluate(), lecture.average());
+    }
+}
