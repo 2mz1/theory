d918a4cdd5c52d323f545411e3a2a6193fb137ab
pursue503 <pursue503@naver.com>
2023-07-22 23:00:59 +0900

    Add Chapter13 liskov client description

--- objects/pursue503/chapter13/src/liskov/v1/Client.java
+++ objects/pursue503/chapter13/src/liskov/v1/Client.java
@@ -13,7 +13,17 @@ public class Client {
         Square square = new Square(10, 10, 10);
         Client client = new Client();
 
+        // 상속으로 인해서 Square 를 전달 할 수 있게됨
         client.resize(square, 50, 100);
+        /*
+            Result
+            1. client -> resize
+            2. rectangle의 width와 height 를 변경함
+            3. 하지만 실제 객체는 정사각혐
+            4. 정사각형의 조건을 무너뜨리게 됨
+            5. 상속을 잘못 사용하게되는 사례
+         */
+
     }
 
 }
